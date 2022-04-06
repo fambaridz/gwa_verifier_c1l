4b2be7dbf4de65814941f836e1fbf295149ad8e0
Xyrk319 <jafabula@up.edu.ph>
2022-04-07 00:09:50 +0800

    Feat: added the initial tables

--- database/gwa_verifier_cmsc_128_db.sql
+++ database/gwa_verifier_cmsc_128_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 06, 2022 at 05:33 PM
+-- Generation Time: Apr 06, 2022 at 06:08 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.28
 
@@ -20,6 +20,109 @@ SET time_zone = "+00:00";
 --
 -- Database: `gwa_verifier_cmsc_128`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `committee`
+--
+
+CREATE TABLE `committee` (
+  `email` varchar(50) NOT NULL,
+  `account_made_by` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `lastname` varchar(50) NOT NULL,
+  `firstname` varchar(50) NOT NULL,
+  `middlename` varchar(50) NOT NULL,
+  `suffix` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `committee_student`
+--
+
+CREATE TABLE `committee_student` (
+  `committee_email` varchar(50) NOT NULL,
+  `student_number` int(20) NOT NULL,
+  `comments` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE `student` (
+  `student_number` int(20) NOT NULL,
+  `lastname` varchar(50) NOT NULL,
+  `firstname` varchar(50) NOT NULL,
+  `middlename` varchar(50) NOT NULL,
+  `suffix` varchar(10) DEFAULT NULL,
+  `degree_program` varchar(50) NOT NULL,
+  `reccommended_number_units` int(20) NOT NULL,
+  `credited_units` int(20) NOT NULL,
+  `gwa` float NOT NULL,
+  `status` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_record`
+--
+
+CREATE TABLE `student_record` (
+  `id` int(50) NOT NULL,
+  `student_number` int(20) NOT NULL,
+  `course_number` int(20) NOT NULL,
+  `grade` int(10) NOT NULL,
+  `units` float NOT NULL,
+  `enrolled` float NOT NULL,
+  `running_total` double NOT NULL,
+  `term` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `committee`
+--
+ALTER TABLE `committee`
+  ADD PRIMARY KEY (`email`);
+
+--
+-- Indexes for table `committee_student`
+--
+ALTER TABLE `committee_student`
+  ADD PRIMARY KEY (`committee_email`,`student_number`);
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`student_number`);
+
+--
+-- Indexes for table `student_record`
+--
+ALTER TABLE `student_record`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `student_number` (`student_number`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `student_record`
+--
+ALTER TABLE `student_record`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
