0acc8fcdd7463b374caa878755e45c55b8ac2381
Ian Salazar <iisalazar@up.edu.ph>
2022-04-25 21:52:12 +0800

    fix: tried to patch issue where the app starts longer than usual
    - added a listener to "did-frame-finish-load" before opening the devtools

--- gwa-verifier-app/src/main.js
+++ gwa-verifier-app/src/main.js
@@ -1,9 +1,11 @@
-import installExtension, { REACT_DEVELOPER_TOOLS } from 'electron-devtools-installer';
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
+import installExtension, {
+  REACT_DEVELOPER_TOOLS,
+} from "electron-devtools-installer";
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) {
+if (require("electron-squirrel-startup")) {
   // eslint-disable-line global-require
   app.quit();
 }
@@ -19,24 +21,26 @@ const createWindow = () => {
   mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  mainWindow.webContents.on("did-frame-finish-load", () => {
+    mainWindow.webContents.openDevTools();
+  });
 };
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on("ready", createWindow);
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('activate', () => {
+app.on("activate", () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
@@ -46,8 +50,8 @@ app.on('activate', () => {
 
 app.whenReady().then(() => {
   installExtension(REACT_DEVELOPER_TOOLS)
-      .then((name) => console.log(`Added Extension:  ${name}`))
-      .catch((err) => console.log('An error occurred: ', err));
+    .then((name) => console.log(`Added Extension:  ${name}`))
+    .catch((err) => console.log("An error occurred: ", err));
 });
 
 // In this file you can include the rest of your app's specific main process
