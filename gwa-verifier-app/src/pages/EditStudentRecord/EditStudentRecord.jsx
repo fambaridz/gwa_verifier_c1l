ea2adbba43322d270e20d52d07f94ec1ed4d49f6
krcastillo <krcastillo@up.edu.ph>
2022-04-27 21:22:32 +0800

    Refactor: removed individual NavBar implementation

--- gwa-verifier-app/src/pages/EditStudentRecord/EditStudentRecord.jsx
+++ gwa-verifier-app/src/pages/EditStudentRecord/EditStudentRecord.jsx
@@ -21,48 +21,6 @@ function EditStudentRecord() {
   const { id } = params;
   return (
     <div>
-      <Box>
-        <AppBar position="static" style={{ background: '#AFAFAF' }}>
-          <Toolbar>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              Verifier
-            </Typography>
-            <Typography variant="h6" style={{ fontWeight: 800 }} component="div" >
-              IAN SALAZAR
-            </Typography>
-            <div>
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleOpenOptionsMenu}
-                color="inherit"
-              >
-                <ArrowDropDown />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorElUser}
-                anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                open={Boolean(anchorElUser)}
-                onClose={handleCloseOptionsMenu}
-              >
-                <MenuItem onClick={handleCloseOptionsMenu}>Manage Accounts</MenuItem>
-                <MenuItem onClick={handleCloseOptionsMenu}>Sign Out</MenuItem>
-              </Menu>
-            </div>
-          </Toolbar>
-        </AppBar>
-      </Box>
       <Container sx={{ paddingTop: 5, paddingBottom: 5 }}>
         {/* <Typography variant="h1">
           Edit student record page w/ the ff. url parameter: {id}
