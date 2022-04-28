132c5fa76a9b92249016b6464ed4df1df0399d3b
apsegovia <apsegovia@up.edu.ph>
2022-04-28 23:15:47 +0800

    Correction in database name.

--- api-v-one/db-connect.php
+++ api-v-one/db-connect.php
@@ -1,9 +1,11 @@
 <?php
 header("Access-Control-Allow-Origin: *"); //add this CORS header to enable any domain to send HTTP requests to these endpoints:
+header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Methods: *");
 $host = "localhost"; 
 $user = "root"; 
 $password = ""; 
-$dbname = "gwa_verifier_cmsc_128"; 
+$dbname = "gwa_verifier_c1l_db"; 
 $id = '';
 
 //connect to database
@@ -13,4 +15,8 @@ $con = mysqli_connect($host, $user, $password,$dbname);
 if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
 }
+else {
+    echo "connected to server";
+}
 
+$con->close();
