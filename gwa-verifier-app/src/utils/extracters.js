62cb76e7a080f190d8b9c20a94a6a006a401a1b7
Ian Salazar <iisalazar@up.edu.ph>
2022-05-05 18:14:21 +0800

    refactor: util functions for parsing csv files are now asynchronous

--- gwa-verifier-app/src/utils/extracters.js
+++ gwa-verifier-app/src/utils/extracters.js
@@ -4,12 +4,14 @@ import { csvToArray } from "./parsers.js";
  *
  * @param {string} text content of csv file the user uploaded
  */
-export function csvExtracter(text) {
+export async function csvExtracter(text) {
+  console.log("PUTA IS HERE THE PROBLEM");
   let [name, degree, ...rest] = text.split("\n");
   const [lname, fname, mname = "", suffix = ""] = name.split(",");
   [degree] = degree.split(",");
   rest = rest.join("\n");
-  const [grades, terms, gwa, recommended] = csvToArray(rest);
+
+  const [grades, terms, gwa, recommended] = await csvToArray(rest);
 
   return {
     lname,
--- gwa-verifier-app/src/utils/parsers.js
+++ gwa-verifier-app/src/utils/parsers.js
@@ -6,54 +6,60 @@ export function csvToArray(str, delimiter = ",") {
 
   // slice from \n index + 1 to the end of the text
   // use split to create an array of each csv value row
-  let rows = str
-    .trim()
-    .slice(str.indexOf("\n") + 1)
-    .split("\n");
-  const regex = /(l|Il|M)\/[0-9]{2}\/[0-9]{2}/m;
-  const footer = rows.splice(rows.length - 5, 5);
-  console.log(footer);
+  return new Promise((resolve, reject) => {
+    try {
+      let rows = str
+        .trim()
+        .slice(str.indexOf("\n") + 1)
+        .split("\n");
+      const regex = /(l|Il|M)\/[0-9]{2}\/[0-9]{2}/m;
+      const footer = rows.splice(rows.length - 5, 5);
 
-  const [, [, gwa], recommended] = footer.split("\n");
+      const [, [, gwa], recommended] = footer;
 
-  let arr = rows.map(function (row) {
-    const values = row.split(delimiter);
-    let [courseno, grade, units, enrolled, running_total, , term = ""] = values;
+      let arr = rows.map(function (row) {
+        const values = row.split(delimiter);
+        let [courseno, grade, units, enrolled, running_total, , term = ""] =
+          values;
 
-    // regex for term
+        // regex for term
 
-    const match = term.match(regex);
-    if (match) [term] = match;
-    return {
-      courseno,
-      grade,
-      units,
-      enrolled,
-      running_total,
+        const match = term.match(regex);
+        if (match) [term] = match;
+        return {
+          courseno,
+          grade,
+          units,
+          enrolled,
+          running_total,
 
-      term,
-    };
-  });
-
-  // loop through the array once again, but this time we will fill the "term"
-  const newArr = [...arr];
-  const terms = [];
-  arr.forEach((row, idx) => {
-    let { term } = row;
-
-    if (regex.test(term)) {
-      terms.push(term);
-      let index = idx - 1;
-      while (index >= 0 && !regex.test(arr[index].term)) {
-        newArr[index] = {
-          ...newArr[index],
           term,
         };
-        index--;
-        // curr = arr[index];
-      }
+      });
+
+      // loop through the array once again, but this time we will fill the "term"
+      const newArr = [...arr];
+      const terms = [];
+      arr.forEach((row, idx) => {
+        let { term } = row;
+
+        if (regex.test(term)) {
+          terms.push(term);
+          let index = idx - 1;
+          while (index >= 0 && !regex.test(arr[index].term)) {
+            newArr[index] = {
+              ...newArr[index],
+              term,
+            };
+            index--;
+            // curr = arr[index];
+          }
+        }
+      });
+      // return the array
+      resolve([newArr, terms, gwa, recommended]);
+    } catch (error) {
+      reject(error);
     }
   });
-  // return the array
-  return [newArr, terms, gwa, recommended];
 }
