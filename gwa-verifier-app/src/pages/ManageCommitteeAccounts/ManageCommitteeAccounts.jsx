4e8e271777a8c76d2d9eb32db02efb3a75494ebf
krcastillo <krcastillo@up.edu.ph>
2022-05-14 12:04:16 +0800

    feat: Added delete button functionality

--- gwa-verifier-app/src/pages/ManageCommitteeAccounts/ManageCommitteeAccounts.jsx
+++ gwa-verifier-app/src/pages/ManageCommitteeAccounts/ManageCommitteeAccounts.jsx
@@ -18,7 +18,7 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
-import { Add, Edit, Delete, WindowSharp } from "@mui/icons-material";
+import { Add, Edit, Delete, WindowSharp, Email } from "@mui/icons-material";
 import "./ManageCommitteeAccounts.css";
 import { Link, useNavigate } from "react-router-dom";
 import { ThemeProvider } from "@mui/private-theming";
@@ -43,7 +43,7 @@ function ManageCommitteeAccounts() {
 			.then(response => response.json())
 			.then(data => {
 				data.map(item => {
-					dataRows.push(createData(item.email, item.firstname.concat(" " + item.lastname)))
+					dataRows.push(createData(item.email, item.firstname.concat(" " + item.middlename + " " + item.lastname + " " + item.suffix)))
 				});
 				setRow(dataRows)
 			})
@@ -84,6 +84,33 @@ function editUserForm() {
 			</form>;
 }
 
+function deleteUserForm() {
+	const [email, setEmail] = React.useState("");
+
+	async function handleSubmit(event) {
+	  event.preventDefault();
+	  const credentials = {email: email}
+	  fetch(
+		  "http://localhost/backend/committee-api.php",
+		  {
+			  method: "DELETE",
+			  body: JSON.stringify(credentials)
+		  })
+		  .then(data => {
+			  console.log(data)
+		  })
+	  setOpenDelete(false);
+	  window.location.reload()
+	}
+  return <form onSubmit={handleSubmit}>
+  <TextField fullWidth={true} sx={{my:1}} label="Email to be deleted" variant="outlined" onChange={(e) => {
+	  setEmail(e.target.value);
+  }}/>
+  <Button className="modalButton" color="success" variant="contained" onClick={() => setOpenDelete(false)}>Cancel</Button>
+  <Button type="submit" className="deleteConfirm" color="success" variant="contained">Confirm</Button>
+
+  </form>;
+}
 
 	const style = {
 	  position: 'absolute',
@@ -97,6 +124,19 @@ function editUserForm() {
 	  boxShadow: 24,
 	  p: 3,
 	}
+
+	const style2 = {
+		position: 'absolute',
+		top: '50%',
+		left: '50%',
+		transform: 'translate(-50%, -50%)',
+		width: 400,
+		height: 200,
+		bgcolor: 'background.paper',
+		border: '2px solid #000',
+		boxShadow: 24,
+		p: 3,
+	  }
 
 
 	const AddModal = (props) => {
@@ -143,11 +183,32 @@ function editUserForm() {
 		)
 	}
 
+	const DeleteModal = (props) => {
+		const [open, setOpen] = React.useState(false);
+	  return (
+		<Modal
+			open={props.open}
+			onClose={props.handleClose}>
+			<Box sx={style2}>
+			<Typography variant="h5" className="modalTypography">Delete Committee Account</Typography>
+			{deleteUserForm()}
+			</Box>
+			</Modal>
+	  );
+	}
+
+	const DeleteButton = (props) => {
+		return (
+		<Button onClick={props.onClickDelete} variant="text" size="small" startIcon={<Delete color="primary"/>} ></Button>
+		)
+	}
+
 	
 
 	
   const [openAdd, setOpenAdd] = React.useState(false);
   const [openEdit, setOpenEdit] = React.useState(false);
+  const [openDelete, setOpenDelete] = React.useState(false);
  
 
   return (
@@ -188,7 +249,7 @@ function editUserForm() {
 									</TableCell>
 									<TableCell>{row.name}</TableCell>
 									<TableCell><EditButton onClickEdit={() => setOpenEdit(true)}/></TableCell>
-									<TableCell><Delete/></TableCell>
+									<TableCell><DeleteButton onClickDelete={() => setOpenDelete(true)}/></TableCell>
 								</TableRow>
 							))}
 						</TableBody>
@@ -198,6 +259,7 @@ function editUserForm() {
 			</Box>
 		</Box>
 	  	<EditModal open={openEdit} handleClose={() => setOpenEdit(false)}/>
+		<DeleteModal open={openDelete} handleClose={() => setOpenDelete(false)}/>
     </div>
   );
 }
