8b7c3d53b7c33360b2f942600e3aea5c78f7ef6f
Xyrk319 <jafabula@up.edu.ph>
2022-06-02 11:25:10 +0800

    fix: readded the delete of student record in studentlist.php

--- gwa-verifier-backend/record-list-api/studentList.php
+++ gwa-verifier-backend/record-list-api/studentList.php
@@ -80,6 +80,14 @@ else if ($method == 'POST') {
         $new[] = $r; 
     }
 
+    // delete from student-record
+    $sql = "DELETE FROM `student_record` WHERE student_number = ?";
+    $stmt = mysqli_stmt_init($con);
+    mysqli_stmt_prepare($stmt, $sql);
+    mysqli_stmt_bind_param($stmt, "i", $target->target);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+
     //Update comments in committee_student
     $sql = "DELETE FROM `committee_student` WHERE student_number = ?";
     $stmt = mysqli_stmt_init($con);
