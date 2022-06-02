f59766535034f51c6b7b388ee19d52d8266f582e
Ian Salazar <iisalazar@up.edu.ph>
2022-06-02 09:53:30 +0800

    feat gwa.php: added statement to handle pre-flight requests

--- gwa-verifier-backend/record-details-api/gwa.php
+++ gwa-verifier-backend/record-details-api/gwa.php
@@ -4,15 +4,21 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
 header("Access-Control-Allow-Methods: *");
-$host = "localhost"; 
-$user = "root"; 
-$password = ""; 
+$host = "localhost";
+$user = "root";
+$password = "";
 $dbname = "gwa_verifier_c1l_db";
- 
-$con = mysqli_connect($host, $user, $password,$dbname);
- 
+
+$con = mysqli_connect($host, $user, $password, $dbname);
+
 $method = $_SERVER['REQUEST_METHOD'];
 $body = json_decode(file_get_contents('php://input'));
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+  header("HTTP/1.1 200 OK");
+  return;
+}
+
 // expected method and body:
 // POST, { term: <"all" or specific semester>, student_number: <student no.> }
 
@@ -20,7 +26,7 @@ if (!$con) {
   die("Connection failed: " . mysqli_connect_error());
 }
 
-switch($body->term) {
+switch ($body->term) {
   case 'all':
     // get grades for all terms
     $sql = "SELECT units, enrolled FROM student_record WHERE student_number = ?";
@@ -40,22 +46,22 @@ switch($body->term) {
     $result = mysqli_stmt_get_result($stmt);
     break;
 }
- 
+
 // die if SQL statement failed
 if (!$result) {
   http_response_code(404);
   die(mysqli_error($con));
 }
 
-if($method == "POST") {
+if ($method == "POST") {
   $total_enrolled = 0;
   $total_units = 0;
 
   // get summation of each course's enrolled and units
-  for($i=0; $i<mysqli_num_rows($result); $i++) {
+  for ($i = 0; $i < mysqli_num_rows($result); $i++) {
     $course = mysqli_fetch_object($result);
-    if($course->enrolled != 0) {
-      if(preg_match("/\(.*\)/", $course->units)) {
+    if ($course->enrolled != 0) {
+      if (preg_match("/\(.*\)/", $course->units)) {
         // for units of format (1)6 or similar
         $total_units += 6;
       } else {
@@ -69,7 +75,7 @@ if($method == "POST") {
   $response = new stdClass();
   $response->total_enrolled = $total_enrolled;
   $response->total_units = $total_units;
-  $response->gwa = $total_enrolled/$total_units;
+  $response->gwa = $total_enrolled / $total_units;
 
   // send response object to frontend
   // response: {total_enrolled: <val>, total_units: <val>, gwa: <val>}
