374f4077cac436b4ae42a83032890fd3d9b2859d
Ian Salazar <iisalazar@up.edu.ph>
2022-06-03 11:44:19 +0800

    fix add-row: fixed bug where adding a row breaks the payload
    - I groupped the records by term before sending a payload to verify it
    - the terms are sorted by year start and year end and then sorted by semester (I, II, and M) with M or midyear being the highest and I or first sem being the lowest
    
    In connection with issue #13

--- gwa-verifier-app/src/utils/transformers.js
+++ gwa-verifier-app/src/utils/transformers.js
@@ -1,3 +1,85 @@
 export function fromMapToArray(obj, keyName) {
   return Object.keys(obj).map((key) => ({ ...obj[key], [keyName]: key }));
 }
+
+/**
+ * @param {I | II | M} sem
+ * @returns {number} a numerical equivalent of the semester
+ */
+function convertSemToNumeric(sem) {
+  const semNumeric = {
+    I: 1,
+    II: 2,
+    M: 3,
+  };
+  if (!Object.keys(semNumeric).includes(sem)) return -1;
+
+  return semNumeric[sem];
+}
+
+/**
+ *
+ * @param {string} term The term of a grade record, must follow the regex /^(I|II|M)\/[0-9]{2}\/[0-9]{2}$/
+ */
+function extractData(term) {
+  const termRegex = /^(I|II|M)\/([0-9]{2})\/([0-9]{2})$/;
+  if (!termRegex.test(term)) {
+    throw new Error(`term '${term}' is not a valid term`);
+  }
+
+  const match = termRegex.exec(term);
+  const [, ...matches] = match;
+  if (matches.length !== 3) {
+    throw new Error(`term '${term}' is not a valid term`);
+  }
+
+  const [sem, startYear, endYear] = matches;
+  return { sem, startYear, endYear };
+}
+
+/**
+ *
+ * @param {Array<{
+ *  courseno: string;
+ *  total: string;
+ *  grade: string;
+ *  units: string;
+ *  enrolled: string;
+ *  term: string;}>} items
+ * @returns {Array<{
+ *  courseno: string;
+ *  total: string;
+ *  grade: string;
+ *  units: string;
+ *  enrolled: string;
+ *  term: string;}>} A copy of the items passed but sorted in terms
+ */
+export function sortGradeRecordsArray(items) {
+  return items.sort(function (a, b) {
+    const { term: term1 } = a;
+    const { term: term2 } = b;
+    try {
+      const semYear1 = extractData(term1);
+
+      const semYear2 = extractData(term2);
+
+      let diff =
+        parseInt(semYear1.startYear) +
+        parseInt(semYear1.endYear) -
+        (parseInt(semYear2.startYear) + parseInt(semYear2.endYear));
+      // if diff === 0
+
+      if (!diff) {
+        const { sem: sem1 } = semYear1;
+
+        const { sem: sem2 } = semYear2;
+
+        return convertSemToNumeric(sem1) - convertSemToNumeric(sem2);
+      }
+      return diff;
+    } catch (error) {
+      console.warn(error.message);
+    }
+    return 0;
+  });
+}
